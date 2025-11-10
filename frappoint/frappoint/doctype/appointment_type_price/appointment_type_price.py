23009699567f80df551f38226fe07f512b1ba886
Emmanuel Mwendwa <mwendwae054@gmail.com>
2025-11-10 22:02:32 +0300

    feat: adds appointment management doctypes and ERPNext dependency
    Introduces core appointment entities for appointment provider, appointment type, appointment service unit, pricing, materials, and settings to enable robust scheduling and resource management.
    
    Renames module to 'Frappoint' and sets ERPNext as a required app for deeper integration with company, item, and pricing data.

---
+++ frappoint/frappoint/doctype/appointment_type_price/appointment_type_price.py
@@ -0,0 +1,9 @@
+# Copyright (c) 2025, Navari LTD and contributors
+# For license information, please see license.txt
+
+# import frappe
+from frappe.model.document import Document
+
+
+class AppointmentTypePrice(Document):
+	pass
---
+++ frappoint/frappoint/doctype/service_appointment_settings/service_appointment_settings.js
@@ -0,0 +1,8 @@
+// Copyright (c) 2025, Navari LTD and contributors
+// For license information, please see license.txt
+
+// frappe.ui.form.on("Service Appointment Settings", {
+// 	refresh(frm) {
+
+// 	},
+// });
