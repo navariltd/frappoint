ff7822c031cdbdd4d46fc14d83237007ec0b6e49
Emmanuel Mwendwa <mwendwae054@gmail.com>
2025-11-17 12:30:53 +0300

    feat: enhances provider form with user sync and validation
    Introduces synchronization of user and employee details
    on the provider form for improved data accuracy.
    Adds validation to prevent duplicate or disabled user assignments,
    and updates field structure to support richer provider information.
    Strengthens practitioner list filtering for active providers.

--- frappoint/frappoint/doctype/appointment_provider/appointment_provider.js
+++ frappoint/frappoint/doctype/appointment_provider/appointment_provider.js
@@ -1,8 +1,128 @@
 // Copyright (c) 2025, Navari LTD and contributors
 // For license information, please see license.txt
 
-// frappe.ui.form.on("Appointment Provider", {
-// 	refresh(frm) {
+frappe.ui.form.on("Appointment Provider", {
+	setup(frm) {
+		frm.set_query("google_calendar", function () {
+			return {
+				filters: {
+					enable: true,
+					owner: frm.doc.user,
+				},
+			};
+		});
+	},
 
-// 	},
-// });
+	user(frm) {
+		if (frm.doc.user) {
+			frappe.call({
+				method: "frappe.client.get",
+				args: {
+					doctype: "User",
+					name: frm.doc.user,
+				},
+				callback: function (data) {
+					frappe.model.get_value(
+						"Employee",
+						{ user_id: frm.doc.user },
+						"name",
+						function (data) {
+							if (data) {
+								if (!frm.doc.employee || frm.doc.employee != data.name)
+									frappe.model.set_value(
+										frm.doctype,
+										frm.docname,
+										"employee",
+										data.name
+									);
+							} else {
+								frappe.model.set_value(frm.doctype, frm.docname, "employee", "");
+							}
+						}
+					);
+
+					if (!frm.doc.first_name || frm.doc.first_name != data.message.first_name)
+						frappe.model.set_value(
+							frm.doctype,
+							frm.docname,
+							"first_name",
+							data.message.first_name
+						);
+					if (!frm.doc.middle_name || frm.doc.middle_name != data.message.middle_name)
+						frappe.model.set_value(
+							frm.doctype,
+							frm.docname,
+							"middle_name",
+							data.message.middle_name
+						);
+					if (!frm.doc.last_name || frm.doc.last_name != data.message.last_name)
+						frappe.model.set_value(
+							frm.doctype,
+							frm.docname,
+							"last_name",
+							data.message.last_name
+						);
+					if (!frm.doc.mobile_no || frm.doc.mobile_no != data.message.mobile_no)
+						frappe.model.set_value(
+							frm.doctype,
+							frm.docname,
+							"mobile_no",
+							data.message.mobile_no
+						);
+				},
+			});
+		}
+	},
+
+	employee: function (frm) {
+		if (frm.doc.employee) {
+			frappe.call({
+				method: "frappe.client.get",
+				args: {
+					doctype: "Employee",
+					name: frm.doc.employee,
+				},
+				callback: function (data) {
+					if (!frm.doc.user || frm.doc.user != data.message.user_id)
+						frm.set_value("user", data.message.user_id);
+					if (!frm.doc.designation || frm.doc.designation != data.message.designation)
+						frappe.model.set_value(
+							frm.doctype,
+							frm.docname,
+							"designation",
+							data.message.designation
+						);
+					if (!frm.doc.grade || frm.doc.grade != data.message.grade)
+						frappe.model.set_value(
+							frm.doctype,
+							frm.docname,
+							"grade",
+							data.message.grade
+						);
+					if (!frm.doc.first_name || !frm.doc.user) {
+						frappe.model.set_value(
+							frm.doctype,
+							frm.docname,
+							"first_name",
+							data.message.first_name
+						);
+						frappe.model.set_value(frm.doctype, frm.docname, "middle_name", "");
+						frappe.model.set_value(
+							frm.doctype,
+							frm.docname,
+							"last_name",
+							data.message.last_name
+						);
+					}
+					if (!frm.doc.mobile_no || !frm.doc.user)
+						frappe.model.set_value(
+							frm.doctype,
+							frm.docname,
+							"mobile_no",
+							data.message.cell_number
+						);
+				},
+			});
+		}
+	},
+});
--- frappoint/frappoint/doctype/appointment_provider/appointment_provider.py
+++ frappoint/frappoint/doctype/appointment_provider/appointment_provider.py
@@ -1,9 +1,80 @@
 # Copyright (c) 2025, Navari LTD and contributors
 # For license information, please see license.txt
 
-# import frappe
+import frappe
+from frappe import _
 from frappe.model.document import Document
 
 
 class AppointmentProvider(Document):
+	# begin: auto-generated types
+	# This code is auto-generated. Do not modify anything in this block.
+
+	from typing import TYPE_CHECKING
+
+	if TYPE_CHECKING:
+		from frappe.types import DF
+
+		active: DF.Check
+		color_code: DF.Color | None
+		designation: DF.Link | None
+		email: DF.Data | None
+		employee: DF.Link | None
+		first_name: DF.Data
+		google_calendar: DF.Link | None
+		grade: DF.Link | None
+		last_name: DF.Data | None
+		middle_name_optional: DF.Data | None
+		mobile_no: DF.Data | None
+		provider_name: DF.Data
+		user: DF.Link | None
+	# end: auto-generated types
 	pass
+
+	def validate(self):
+		self.set_full_name()
+		self.validate_user_id()
+
+	def set_full_name(self):
+		if self.last_name:
+			self.provider_name = " ".join(filter(None, [self.first_name, self.last_name]))
+		else:
+			self.provider_name = self.first_name
+
+	def validate_user_id(self):
+		if not frappe.db.exists("User", self.user):
+			frappe.throw(_("User {0} does not exist").format(self.user))
+		elif not frappe.db.exists("User", self.user, "enabled"):
+			frappe.throw(_("User {0} is disabled").format(self.user))
+
+		# check duplicate
+		provider = frappe.db.exists("Appointment Provider", {"user_id": self.user, "name": ("!=", self.name)})
+		if provider:
+			frappe.throw(
+				_("User {0} is already assigned to Appointment Provider {1}").format(self.user, provider)
+			)
+
+
+@frappe.whitelist()
+@frappe.validate_and_sanitize_search_inputs
+def get_practitioner_list(doctype, txt, searchfield, start, page_len, filters=None):
+	active_filter = {"active": 1}
+
+	filters = {**active_filter, **filters} if filters else active_filter
+
+	fields = ["name", "provider_name", "mobile_no"]
+
+	pattern = f"%{txt}%"
+
+	text_in = {"name": ("like", pattern), "provider_name": ("like", pattern)}
+
+	return frappe.get_all(
+		"Appointment Provider",
+		fields=fields,
+		filters=filters,
+		or_filters=text_in,
+		start=start,
+		page_length=page_len,
+		order_by="name, provider_name",
+		as_list=1,
+	)
