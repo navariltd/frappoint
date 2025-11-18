b095414ded54e97391d181dec781415d462b3b24
Emmanuel Mwendwa <mwendwae054@gmail.com>
2025-11-18 16:04:11 +0300

    feat: add before_tests utility function for setup and cache clearing

--- frappoint/hooks.py
+++ frappoint/hooks.py
@@ -12,6 +12,8 @@ required_apps = ["erpnext"]
 
 export_python_type_annotations = True
 
+before_tests = "frappoint.setup.utils.before_tests"
+
 # Each item in the list will be shown as an app in the apps page
 # add_to_apps_screen = [
 # 	{
@@ -170,8 +172,6 @@ export_python_type_annotations = True
 
 # Testing
 # -------
-
-# before_tests = "frappoint.install.before_tests"
 
 # Overriding Methods
 # ------------------------------
