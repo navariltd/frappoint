bcfad4c0b1725883c35b2b72d8ff822bff13dc3a
Emmanuel Mwendwa <mwendwae054@gmail.com>
2026-01-07 14:00:57 +0300

    refactor: rename document

--- frappoint/frappoint/doctype/appointment_service_unit/appointment_service_unit.py
+++
@@ -1,9 +0,0 @@
-# Copyright (c) 2025, Navari LTD and contributors
-# For license information, please see license.txt
-
-# import frappe
-from frappe.model.document import Document
-
-
-class AppointmentServiceUnit(Document):
-	pass
--- frappoint/frappoint/doctype/service_unit/test_service_unit.py
+++ frappoint/frappoint/doctype/service_unit/test_service_unit.py
@@ -5,5 +5,5 @@
 from frappe.tests.utils import FrappeTestCase
 
 
-class TestAppointmentServiceUnit(FrappeTestCase):
+class TestServiceUnit(FrappeTestCase):
 	pass
