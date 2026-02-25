245781d9fe93bceda114f1075bb746a55b96f208
Emmanuel Mwendwa <mwendwae054@gmail.com>
2026-02-25 10:09:18 +0300

    fix: add payment gateway field to service appointment payment and remove unused mpesa hook

--- frappoint/hooks.py
+++ frappoint/hooks.py
@@ -185,11 +185,6 @@ after_install = "frappoint.setup.setup.execute"
 # ---------------
 # Hook on document methods and events
 
-doc_events = {
-	"Mpesa Express Request": {
-		"on_update": "frappoint.mpesa.hook_mpesa_express_request",
-	}
-}
 
 # Scheduled Tasks
 # ---------------
