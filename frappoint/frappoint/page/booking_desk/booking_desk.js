88045512094ddb608ec37267d9f356a528048846
Emmanuel Mwendwa <mwendwae054@gmail.com>
2026-03-16 16:57:00 +0300

    feat: implement booking desk page with initial setup and configuration

---
+++ frappoint/frappoint/page/booking_desk/booking_desk.js
@@ -0,0 +1,7 @@
+frappe.pages["booking_desk"].on_page_load = function (wrapper) {
+	var page = frappe.ui.make_app_page({
+		parent: wrapper,
+		title: "Frappoint Booking Desk",
+		single_column: true,
+	});
+};
