fd9c24ae773c4763cd429f0096d2c2d37a52d071
Emmanuel Mwendwa <mwendwae054@gmail.com>
2026-03-18 11:36:09 +0300

    feat: implement router and auth store for user authentication and navigation

---
+++ booking_desk/src/router.js
@@ -0,0 +1,45 @@
+import { createRouter, createWebHistory } from "vue-router";
+import { useAuthStore } from "./stores/auth";
+import { navigateTo } from "./utils";
+
+const routes = [
+	{
+		name: "Bookings",
+		path: "/",
+		component: () => import("@/pages/booking/Bookings.vue"),
+		meta: { requiresLogin: true },
+	},
+	{
+		path: "/services",
+		name: "Services",
+		component: () => import("@/pages/services/Services.vue"),
+		meta: { requiresLogin: false },
+	},
+	{
+		name: "Login",
+		path: "/login",
+		meta: { external: true },
+	},
+];
+
+const router = createRouter({
+	history: createWebHistory("/booking_desk"),
+	routes,
+});
+
+router.beforeEach(async (to) => {
+	const auth = useAuthStore();
+
+	await auth.refreshUser?.();
+
+	if (to.meta.requiresLogin && !auth.isLoggedIn) {
+		navigateTo("Login", { redirect: `booking_desk${to.fullPath}` });
+		return false;
+	}
+
+	if (to.name === "Login" && auth.isLoggedIn) {
+		return { name: "Bookings" };
+	}
+});
+
+export default router;
