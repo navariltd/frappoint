52e62fcd7f0c970a5d428e222e84f52c0e9ef8f2
Emmanuel Mwendwa <mwendwae054@gmail.com>
2026-03-31 18:04:34 +0300

    feat: enhance Service Appointment Payment and add Service Appointment Payment Reference doctype with new fields

--- frappoint/frappoint/doctype/service_appointment_payment/service_appointment_payment.py
+++ frappoint/frappoint/doctype/service_appointment_payment/service_appointment_payment.py
@@ -14,15 +14,24 @@ class ServiceAppointmentPayment(Document):
 	if TYPE_CHECKING:
 		from frappe.types import DF
 
+		from frappoint.frappoint.doctype.service_appointment_payment_reference.service_appointment_payment_reference import (
+			ServiceAppointmentPaymentReference,
+		)
+
+		amended_from: DF.Link | None
 		amount: DF.Currency
 		currency: DF.Link | None
+		mode_of_payment: DF.Link
 		name: DF.Int | None
 		order_id: DF.Data | None
 		payment_gateway: DF.Link | None
 		payment_id: DF.Data | None
 		payment_received: DF.Check
-		reference_docname: DF.DynamicLink | None
-		reference_doctype: DF.Link | None
-		user: DF.Link
+		posting_date: DF.Date | None
+		reference_date: DF.Date | None
+		reference_docname: DF.DynamicLink
+		reference_doctype: DF.Link
+		references: DF.Table[ServiceAppointmentPaymentReference]
+		user: DF.Link | None
 	# end: auto-generated types
 	pass
---
+++ frappoint/frappoint/doctype/service_appointment_payment_reference/service_appointment_payment_reference.py
@@ -0,0 +1,27 @@
+# Copyright (c) 2026, Navari LTD and contributors
+# For license information, please see license.txt
+
+# import frappe
+from frappe.model.document import Document
+
+
+class ServiceAppointmentPaymentReference(Document):
+	# begin: auto-generated types
+	# This code is auto-generated. Do not modify anything in this block.
+
+	from typing import TYPE_CHECKING
+
+	if TYPE_CHECKING:
+		from frappe.types import DF
+
+		allocated_amount: DF.Currency
+		currency: DF.Link | None
+		grand_total: DF.Currency
+		outstanding_amount: DF.Currency
+		parent: DF.Data
+		parentfield: DF.Data
+		parenttype: DF.Data
+		reference_doctype: DF.Link
+		reference_name: DF.DynamicLink
+	# end: auto-generated types
+	pass
