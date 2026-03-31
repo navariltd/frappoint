f690fa857d6eb0e9566aa29a112e7e676437a989
Emmanuel Mwendwa <mwendwae054@gmail.com>
2026-03-31 19:05:51 +0300

    feat: enhance Service Appointment Payment with reference handling and validation logic

--- frappoint/frappoint/doctype/service_appointment_payment/service_appointment_payment.js
+++ frappoint/frappoint/doctype/service_appointment_payment/service_appointment_payment.js
@@ -1,8 +1,38 @@
 // Copyright (c) 2026, Navari LTD and contributors
 // For license information, please see license.txt
 
-// frappe.ui.form.on("Service Appointment Payment", {
-// 	refresh(frm) {
+frappe.ui.form.on("Service Appointment Payment", {
+	refresh(frm) {
+		if (frm.doc.docstatus === 0 && frm.doc.reference_doctype === "Service Booking") {
+			frm.add_custom_button(__("Get Appointments"), () => {
+				frm.call("get_references").then(() => {
+					frm.refresh_field("references");
+				});
+			});
+		}
 
-// 	},
-// });
+		frm.set_query("reference_doctype", () => {
+			return {
+				filters: {
+					name: ["in", ["Service Booking", "Service Appointment"]],
+				},
+			};
+		});
+
+		frm.set_query("reference_doctype", "references", () => {
+			return {
+				filters: {
+					name: ["in", ["Service Appointment"]],
+				},
+			};
+		});
+	},
+
+	reference_docname: function (frm) {
+		if (frm.doc.reference_docname) {
+			frm.call("get_reference_details").then(() => {
+				frm.refresh_fields(["amount", "currency"]);
+			});
+		}
+	},
+});
--- frappoint/frappoint/doctype/service_appointment_payment/service_appointment_payment.py
+++ frappoint/frappoint/doctype/service_appointment_payment/service_appointment_payment.py
@@ -1,8 +1,9 @@
 # Copyright (c) 2026, Navari LTD and contributors
 # For license information, please see license.txt
 
-# import frappe
+import frappe
 from frappe.model.document import Document
+from frappe.utils import flt
 
 
 class ServiceAppointmentPayment(Document):
@@ -34,4 +35,79 @@ class ServiceAppointmentPayment(Document):
 		references: DF.Table[ServiceAppointmentPaymentReference]
 		user: DF.Link | None
 	# end: auto-generated types
-	pass
+
+	def validate(self):
+		if self.reference_doctype not in ["Service Booking", "Service Appointment"]:
+			frappe.throw("Not Supported")
+
+		if self.references:
+			for reference in self.references:
+				if reference.reference_doctype not in ["Service Appointment"]:
+					frappe.throw("Not Supported")
+
+		if self.reference_doctype and self.reference_docname and not self.amount:
+			self.get_reference_details()
+
+		# If paying for a Booking but references table is empty, populate it
+		if self.reference_doctype == "Service Booking" and not self.references:
+			self.get_references()
+
+	def on_submit(self):
+		self.update_outstanding_balances(cancel=False)
+
+	def on_cancel(self):
+		self.update_outstanding_balances(cancel=True)
+
+	def update_outstanding_balances(self, cancel=False):
+		self.adjust_doc_outstanding(self.reference_doctype, self.reference_docname, self.amount, cancel)
+
+		if self.reference_doctype == "Service Booking":
+			for ref in self.references:
+				self.adjust_doc_outstanding(
+					ref.reference_doctype, ref.reference_name, ref.allocated_amount, cancel
+				)
+
+	def adjust_doc_outstanding(self, doctype, docname, amount, cancel):
+		change = flt(amount) if cancel else -flt(amount)
+
+		new_outstanding = flt(frappe.db.get_value(doctype, docname, "outstanding_amount")) + change
+
+		frappe.db.set_value(doctype, docname, "outstanding_amount", new_outstanding)
+
+	@frappe.whitelist()
+	def get_reference_details(self):
+		"""Fetch amount and currency from the source document"""
+		ref_doc = frappe.get_doc(self.reference_doctype, self.reference_docname)
+		self.currency = ref_doc.currency
+		self.amount = ref_doc.outstanding_amount
+
+	@frappe.whitelist()
+	def get_references(self):
+		"""Populate the child table with linked appointments"""
+		if self.reference_doctype != "Service Booking":
+			return
+
+		self.set("references", [])
+
+		appointments = frappe.get_all(
+			"Service Appointment",
+			filters={
+				"booking_id": self.reference_docname,
+				"status": ["not in", ["Cancelled"]],
+				"outstanding_amount": [">", 0],
+			},
+			fields=["name", "grand_total", "outstanding_amount", "currency"],
+		)
+
+		for appt in appointments:
+			self.append(
+				"references",
+				{
+					"reference_doctype": "Service Appointment",
+					"reference_name": appt.name,
+					"currency": appt.currency,
+					"grand_total": appt.grand_total,
+					"outstanding_amount": appt.outstanding_amount,
+					"allocated_amount": appt.outstanding_amount,
+				},
+			)
